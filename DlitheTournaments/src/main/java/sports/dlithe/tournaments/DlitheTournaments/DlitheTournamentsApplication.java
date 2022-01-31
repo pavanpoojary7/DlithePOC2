2ccdd48f210622d342ae5dbe2d8dab157fecf237
pavanpoojary <pavanpoojary17@gmail.com>
2022-01-31 13:04:51 +0530

    first

---
+++ DlitheTournaments/src/main/java/sports/dlithe/tournaments/DlitheTournaments/DlitheTournamentsApplication.java
@@ -0,0 +1,13 @@
+package sports.dlithe.tournaments.DlitheTournaments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlitheTournamentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlitheTournamentsApplication.class, args);
+	}
+
+}
---
+++ DlitheTournaments/src/test/java/sports/dlithe/tournaments/DlitheTournaments/DlitheTournamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package sports.dlithe.tournaments.DlitheTournaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlitheTournamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
