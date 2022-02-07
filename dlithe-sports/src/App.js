212d3c29632ec2f6c3daa677d47422933b2ad421
pavanpoojary <pavanpoojary17@gmail.com>
2022-02-07 13:05:22 +0530

    past details

--- dlithe-sports/src/App.js
+++ dlithe-sports/src/App.js
@@ -5,7 +5,7 @@ import { NewTournament } from './NewTournament';
 import {Home } from './Home'
 import { Athlete } from './Athlete';
 import { Participants } from './Participants';
-
+import {Past} from './Past';
 
 const App=()=>{
 
@@ -18,6 +18,7 @@ const App=()=>{
           <Route path="/new" exact element={<NewTournament/>}/>
           <Route path="/ath/:position" exact element={<Athlete/>}/>
           <Route path="/show/:position" exact element={<Participants/>}/>
+          <Route path="/completed" exact element={<Past/>}/>
    </Routes>
    
    </BrowserRouter>
